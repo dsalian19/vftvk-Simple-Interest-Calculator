6bc4f98414f0037943b20605f1db2924e810e9ab
Deepak Salian <deepak@Deepaks-MacBook-Pro.local>
2021-06-27 12:30:51 -0500

    edited index, style, and script files to complete calculator

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //getting values from HTML element IDs
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = parseInt(document.getElementById("years").value);
     
+    //Calculate interest
+    var interest = principal * years * rate /100;
+
+    //making yearFuture a future date based on inputs
+    var yearFuture = new Date().getFullYear() + years;
+    document.getElementById("result").innerHTML = "<br/>If you deposit <mark>" + principal + "</mark>,<br/>" + "at an interest rate of <mark>" + rate + "%</mark>,<br/>" +
+    "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +  "in the year <mark>" + yearFuture + "</mark>";
 }
-        
+
+function updateRate() 
+{
+    //read rate value and displays
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validate(){
+    //getting value
+    var principal = document.getElementById("principal").value
+
+    //if input value is below 1 give an alert and focus. 
+    if (parseInt(principal) < 1){
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    } 
+}
